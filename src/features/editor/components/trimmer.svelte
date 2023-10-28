dc41729b576ab1d78033b6ad33a6c5acd76754ea
jrafaaael <jrmd070102@gmail.com>
2023-10-28 17:39:16 -0400

    refactor: use `footer` in `editor` directly

---
+++ src/features/editor/components/trimmer.svelte
@@ -0,0 +1,10 @@
+<div
+  class="w-full h-10 bg-blue-500/30 rounded-md flex justify-between items-center overflow-hidden"
+>
+  <button class="w-auto h-full px-2">
+    <div class="w-1 h-1/2 bg-white/50 rounded-full" />
+  </button>
+  <button class="w-auto h-full px-2">
+    <div class="w-1 h-1/2 bg-white/50 rounded-full" />
+  </button>
+</div>
--- src/features/editor/editor.svelte
+++ src/features/editor/editor.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { video } from "../../stores/video.store";
-  import Controls from "./components/controls.svelte";
   import Header from "./components/header.svelte";
+  import Timeline from "./components/timeline.svelte";
+  import Trimmer from "./components/trimmer.svelte";
 </script>
 
 <main class="w-full h-full bg-neutral-950 flex flex-col">
@@ -10,5 +11,10 @@
     <!-- svelte-ignore a11y-media-has-caption -->
     <video src={$video.blobUrl} />
   </section>
-  <Controls />
+  <footer class="w-full bg-neutral-900 border-t-2 border-t-white/5">
+    <Timeline />
+    <div class="w-full py-5 px-10 bg-neutral-950 flex flex-col gap-5">
+      <Trimmer />
+    </div>
+  </footer>
 </main>
