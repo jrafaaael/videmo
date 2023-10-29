08dd6cd2ca3af7b03f97872d5f59dc1f0e1aeec8
jrafaaael <jrmd070102@gmail.com>
2023-10-29 16:24:58 -0400

    fix: keep only int

--- src/features/editor/utils/seconds-to-time.ts
+++ src/features/editor/utils/seconds-to-time.ts
@@ -1,5 +1,6 @@
 export function secondsToTime(time: number) {
-  const minutes = Math.floor(time / 60);
+  time = +time.toFixed(0);
+  const minutes = Math.floor(+time.toFixed(0) / 60);
   const seconds = time - minutes * 60;
 
   return `${minutes.toString(10).padStart(2, "0")}:${seconds
