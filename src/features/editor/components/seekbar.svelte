d707d344bb2d9652d20557988847a6dbc5515023
jrafaaael <jrmd070102@gmail.com>
2023-11-01 11:52:00 -0400

    refactor: calculate timeline gap and seekbar position in css percentage instead of pixels based on `ResizeObserver`

--- src/features/editor/components/seekbar.svelte
+++ src/features/editor/components/seekbar.svelte
@@ -1,26 +1,18 @@
 <script lang="ts">
-  import ResizeObserver from "../../../components/resize-observer.svelte";
   import { recording } from "../../../stores/recording.store";
-  import { secondsToPixels } from "../utils/seconds-to-pixels";
 
   export let time: number;
-  let seekbarRef: HTMLDivElement;
+  $: position = (time * 100) / $recording.duration;
 </script>
 
-<ResizeObserver element={seekbarRef?.parentElement} let:inlineSize>
-  <div
-    class="h-[calc(100%+8px)] absolute bottom-0 z-50 transition-all ease-linear cursor-col-resize"
-    style="left: {secondsToPixels({
-      sec: time,
-      totalPixels: inlineSize,
-      totalSeconds: $recording.duration,
-    })}px;"
-  >
-    <div class="h-full relative">
-      <div
-        class="w-2 aspect-square bg-orange-hard rounded-full absolute left-1/2 -translate-x-1/2"
-      />
-      <div class="w-0.5 h-full bg-orange-hard" />
-    </div>
+<div
+  class="h-[calc(100%+8px)] absolute bottom-0 z-50 transition-all ease-linear cursor-col-resize"
+  style="left: {position}%;"
+>
+  <div class="h-full relative">
+    <div
+      class="w-2 aspect-square bg-orange-hard rounded-full absolute left-1/2 -translate-x-1/2"
+    />
+    <div class="w-0.5 h-full bg-orange-hard" />
   </div>
-</ResizeObserver>
+</div>
--- src/features/editor/components/timeline.svelte
+++ src/features/editor/components/timeline.svelte
@@ -1,27 +1,25 @@
 <script lang="ts">
-  import ResizeObserver from "../../../components/resize-observer.svelte";
   import { recording } from "../../../stores/recording.store";
-  import { secondsToPixels } from "../utils/seconds-to-pixels";
   import { secondsToTime } from "../utils/seconds-to-time";
 
-  let seconds = [...Array(Math.floor($recording.duration) + 1).keys()];
+  let greatestSecondsLessThanDuration = Math.floor($recording.duration);
+  let seconds = [...Array(greatestSecondsLessThanDuration + 1).keys()];
+  let availableWidth =
+    (greatestSecondsLessThanDuration * 100) / $recording.duration;
 </script>
 
-<ResizeObserver class="w-full" let:inlineSize>
-  {@const gap = secondsToPixels({
-    sec: 1,
-    totalPixels: inlineSize,
-    totalSeconds: $recording.duration,
-  })}
-  <div class="w-full flex items-end" style="gap: {gap}px">
-    {#each seconds as num}
-      <div class="flex flex-col items-center relative z-10">
-        <span class="text-xs text-white/40 absolute -top-4">
-          {secondsToTime(num)}
-        </span>
-        <div class="w-0.5 h-4 bg-white/20 translate-y-1/2" />
-      </div>
-      <!-- {#if num % 10 === 0}
+<div
+  class="w-full max-w-[100vw] flex items-end"
+  style="gap: calc(({availableWidth}%/{seconds.length - 1}) - 2px)"
+>
+  {#each seconds as num}
+    <div class="flex flex-col items-center relative z-10">
+      <span class="text-xs text-white/40 absolute -top-4">
+        {secondsToTime(num)}
+      </span>
+      <div class="w-0.5 h-4 bg-white/20 translate-y-1/2" />
+    </div>
+    <!-- {#if num % 10 === 0}
       <div class="flex flex-col items-center relative z-10">
         <span class="text-xs text-white/40 absolute -top-4">
           {secondsToTime(num)}
@@ -31,6 +29,5 @@
     {:else if num % 5 === 0}
       <div class="w-0.5 h-2 bg-white/20 translate-y-0.5" />
     {/if} -->
-    {/each}
-  </div>
-</ResizeObserver>
+  {/each}
+</div>
