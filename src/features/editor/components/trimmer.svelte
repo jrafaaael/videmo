269990a8e2293fe3ab1d9cd14ddbc0f7a4b73e2d
jrafaaael <jrmd070102@gmail.com>
2023-11-06 16:10:29 -0400

    fix: increase padding of trimmer resizer

--- src/features/editor/components/trimmer.svelte
+++ src/features/editor/components/trimmer.svelte
@@ -1,10 +1,10 @@
 <div
   class="w-full h-10 bg-blue-500/30 rounded-md flex justify-between items-center overflow-hidden"
 >
-  <button class="w-auto h-full px-2">
+  <button class="w-auto h-full pr-4 pl-2 cursor-col-resize">
     <div class="w-1 h-1/2 bg-white/50 rounded-full" />
   </button>
-  <button class="w-auto h-full px-2">
+  <button class="w-auto h-full pr-2 pl-4 cursor-col-resize">
     <div class="w-1 h-1/2 bg-white/50 rounded-full" />
   </button>
 </div>
