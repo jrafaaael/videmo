32d4eab9b6fdcdd6a3189f4b655c221485871c73
jrafaaael <jrmd070102@gmail.com>
2023-11-08 12:28:34 -0400

    refactor: header styles

--- src/features/editor/components/header.svelte
+++ src/features/editor/components/header.svelte
@@ -4,14 +4,13 @@
   import { recording } from "../../../stores/recording.store";
 
   function handleRetry() {
-    const blobUrl = $recording.blobUrl;
-    URL.revokeObjectURL(blobUrl);
+    URL.revokeObjectURL($recording.blobUrl);
     recording.set(null);
   }
 </script>
 
 <header
-  class="h-16 px-10 bg-neutral-800 border-b-2 border-b-white/5 shrink-0 flex justify-between items-center"
+  class="h-16 px-10 bg-neutral-800 border-b-2 border-b-white/5 shrink-0 flex justify-between items-center gap-8"
 >
   <button
     class="p-2 rounded-md text-neutral-300 transition-all hover:bg-white/[0.075] hover:text-neutral-200"
@@ -21,9 +20,11 @@
       {@html Trash}
     </div>
   </button>
-  <h1 class="text-xl font-bold">
-    <span>{$recording.id}</span>
-    <span class="text-neutral-500">.webm</span>
+  <h1 class="text-xl font-bold flex">
+    <span class="line-clamp-1 break-all grow shrink-0 basis-0">
+      {$recording.id}
+    </span>
+    <span class="text-neutral-500 grow-0 shrink basis-1">.webm</span>
   </h1>
   <a
     class="py-1 px-3 bg-purple-600 rounded-md flex items-center gap-2"
