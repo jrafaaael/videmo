d4894a9dcb45db1fdeb922431061c069d5496d5e
jrafaaael <jrmd070102@gmail.com>
2023-11-13 00:29:39 -0400

    wip: trim video with ffmpeg

--- src/features/editor/components/header.svelte
+++ src/features/editor/components/header.svelte
@@ -1,39 +1,65 @@
 <script lang="ts">
+  import { fetchFile } from "@ffmpeg/util";
   import Trash from "../../../components/icons/trash.svg?raw";
   import Download from "../../../components/icons/download.svg?raw";
   import { recording } from "../../../stores/recording.store";
+  import { ffmpeg } from "../../../stores/ffmpeg.store";
 
-  function handleRetry() {
-    URL.revokeObjectURL($recording.blobUrl);
-    recording.set(null);
+  async function process() {
+    console.log("here");
+    try {
+      await $ffmpeg.writeFile("input.webm", await fetchFile($recording.url));
+      await $ffmpeg.exec([
+        "-ss",
+        "0",
+        "-to",
+        "3",
+        "-i",
+        "input.webm",
+        "-c",
+        "copy",
+        "-copyts",
+        $recording.id + ".webm",
+      ]);
+      const video = await $ffmpeg.readFile($recording.id + ".webm");
+      const downloadElement = document.createElement("a");
+      const url = URL.createObjectURL(new Blob([video], { type: "video/webm" }));
+      downloadElement.href = url;
+      downloadElement.download = $recording.id + ".webm";
+      downloadElement.click();
+      URL.revokeObjectURL(url);
+      console.log("here2");
+    } catch (e) {
+      console.log(e);
+    }
   }
 </script>
 
 <header
   class="h-16 px-10 bg-neutral-800 border-b-2 border-b-white/5 shrink-0 flex justify-between items-center gap-8"
 >
-  <button
+  <a
     class="p-2 rounded-md text-neutral-300 transition-all hover:bg-white/[0.075] hover:text-neutral-200"
-    on:click={handleRetry}
+    href="/"
+    data-astro-reload
   >
     <div class="w-4 aspect-square">
       {@html Trash}
     </div>
-  </button>
+  </a>
   <h1 class="text-xl font-bold flex">
     <span class="line-clamp-1 break-all grow shrink-0 basis-0">
       {$recording.id}
     </span>
     <span class="text-neutral-500 grow-0 shrink basis-1">.webm</span>
   </h1>
-  <a
+  <button
     class="py-1 px-3 bg-purple-600 rounded-md flex items-center gap-2"
-    href={$recording.blobUrl}
-    download={$recording.id}
+    on:click={process}
   >
     <div class="w-5 aspect-square">
       {@html Download}
     </div>
     <span>Save</span>
-  </a>
+  </button>
 </header>
