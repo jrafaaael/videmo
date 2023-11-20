832ce809e887f1f199dad016d7ccc47659fcabd9
jrafaaael <jrmd070102@gmail.com>
2023-11-19 20:58:37 -0400

    wip: sidebar

---
+++ src/routes/[id]/components/sidebar.svelte
@@ -0,0 +1,26 @@
+<script>
+	let padding = 0;
+
+	// function redraw() {
+	// 	const MINIMUM_PADDING = 100;
+	// 	const ctx = canvasRef?.getContext('2d');
+	// 	const scale = canvasRef?.width / canvasRef?.height;
+	// 	const p = padding * 4;
+	// 	const width = canvasRef?.width - p - MINIMUM_PADDING;
+	// 	const height = (canvasRef?.width - p) / scale;
+	// 	const left = (canvasRef?.width - width) / 2;
+	// 	const top = (canvasRef?.height - height) / 2;
+
+	// 	ctx!.fillStyle = 'blue';
+	// 	ctx!.fillRect(0, 0, canvasRef.width, canvasRef.height);
+	// 	ctx?.drawImage(videoRef, left, top, width, height);
+	// }
+
+	// function updateCanvas() {
+	// 	redraw();
+
+	// 	if (!paused) {
+	// 		animationId = window?.requestAnimationFrame(updateCanvas);
+	// 	}
+	// }
+</script>
