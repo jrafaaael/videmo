3650774c4eea299552b079e001e6bf3251529873
jrafaaael <jrmd070102@gmail.com>
2023-11-25 18:47:11 -0400

    fix: duplicate `<html>` tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,7 @@
 	import '$lib/styles/tailwind.css';
 </script>
 
-<html lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-		<meta name="viewport" content="width=device-width" />
-		<title>Videmo</title>
-	</head>
-	<body class="w-full h-[100svh] bg-neutral-800 text-neutral-50">
-		<slot />
-	</body>
-</html>
+<svelte:head>
+	<title>Videmo</title>
+</svelte:head>
+<slot />
