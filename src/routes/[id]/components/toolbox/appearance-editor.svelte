8f76e7ea9b30ed6ba4a9869f83c65863ea19d2fa
jrafaaael <jrmd070102@gmail.com>
2023-11-26 10:04:49 -0400

    wip: background and appearence sections

---
+++ src/routes/[id]/components/toolbox/appearance-editor.svelte
@@ -0,0 +1,1 @@
+<h6>appearance EDITOR BITCH</h6>
--- src/routes/[id]/components/toolbox/toolbox.svelte
+++ src/routes/[id]/components/toolbox/toolbox.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
 	import * as Accordion from '$lib/components/accordion';
 	import ChevronDown from '../icons/chevron-down.svelte';
+	import BackgroundEditor from './background-editor.svelte';
+	import AppearanceEditor from './appearance-editor.svelte';
 
 	const options = [
 		{
 			id: 'background',
 			label: 'Background',
-			content: 'BackgroundEditor'
+			content: BackgroundEditor
 		},
 		{
 			id: 'appearance',
 			label: 'Appearance',
-			content: 'BackgroundEditor'
+			content: AppearanceEditor
 		}
 	];
 </script>
@@ -31,7 +33,7 @@
 						{item.label}
 					</Accordion.Trigger>
 					<Accordion.Content class="px-4 pb-4" id={item.id}>
-						{item.content}
+						<svelte:component this={item.content} />
 					</Accordion.Content>
 				</Accordion.Item>
 			{/each}
