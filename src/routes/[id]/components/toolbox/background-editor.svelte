3ac676bf793c5d4b7405a28ed06b512356d976c7
jrafaaael <jrmd070102@gmail.com>
2023-11-27 10:38:14 -0400

    refactor: import wallpapers from `$lib`

--- src/routes/[id]/components/toolbox/background-editor.svelte
+++ src/routes/[id]/components/toolbox/background-editor.svelte
@@ -1,30 +1,27 @@
 <script lang="ts">
-	const WALLPAPERS_PATH = '/images/wallpapers';
-	const wallpapers = ['big-sur.webp', 'sonoma.webp', 'ventura.webp'] as const;
+	import { WALLPAPERS } from '../../utils/wallpapers';
 
-	let selected = wallpapers.at(0);
+	let selected = WALLPAPERS.at(0)?.name;
 </script>
 
 <div class="flex flex-wrap gap-4">
-	{#each wallpapers as wallpaper}
-		{@const wallpaperName = wallpaper.split('.').at(0)}
-
+	{#each WALLPAPERS as wallpaper}
 		<label
-			for={wallpaperName}
 			class="rounded-lg overflow-hidden outline outline-2 outline-offset-2 cursor-pointer transition-transform hover:scale-95 focus-within:scale-95
 			{selected === wallpaper.name ? 'outline-white/90 hover:scale-100' : 'outline-transparent'}"
+			for={wallpaper.name}
 		>
 			<img
 				class="w-10 aspect-square object-fill"
-				src="{WALLPAPERS_PATH}/{wallpaper}"
-				alt="{wallpaperName} wallpaper"
+				src={wallpaper.url}
+				alt="{wallpaper.name} wallpaper"
 			/>
 			<input
 				type="radio"
 				class="sr-only"
 				name="wallpaper"
-				value={wallpaper}
-				id={wallpaperName}
+				value={wallpaper.name}
+				id={wallpaper.name}
 				bind:group={selected}
 			/>
 		</label>
---
+++ src/routes/[id]/utils/wallpapers.ts
@@ -0,0 +1,9 @@
+import bigSur from '$lib/assets/images/wallpapers/big-sur.webp';
+import sonoma from '$lib/assets/images/wallpapers/sonoma.webp';
+import ventura from '$lib/assets/images/wallpapers/ventura.webp';
+
+export const WALLPAPERS = [
+	{ name: 'bigSur', url: bigSur },
+	{ name: 'sonoma', url: sonoma },
+	{ name: 'ventura', url: ventura }
+] as const;
