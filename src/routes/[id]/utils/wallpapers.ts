837492f0ec1a2394a51f6f95c573abc905cb7d03
jrafaaael <jrmd070102@gmail.com>
2023-11-27 23:52:23 -0400

    feat: add `monterey` wallpaper

--- src/routes/[id]/utils/wallpapers.ts
+++ src/routes/[id]/utils/wallpapers.ts
@@ -1,9 +1,11 @@
 import bigSur from '$lib/assets/images/wallpapers/big-sur.webp';
 import sonoma from '$lib/assets/images/wallpapers/sonoma.webp';
 import ventura from '$lib/assets/images/wallpapers/ventura.webp';
+import monterey from '$lib/assets/images/wallpapers/monterey.webp';
 
 export const WALLPAPERS = [
 	{ name: 'bigSur', url: bigSur },
 	{ name: 'sonoma', url: sonoma },
-	{ name: 'ventura', url: ventura }
+	{ name: 'ventura', url: ventura },
+	{ name: 'monterey', url: monterey }
 ] as const;
