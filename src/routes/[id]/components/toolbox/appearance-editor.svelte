398769c03e1e4a747bf2bc98e69cd612fe23ff83
jrafaaael <jrmd070102@gmail.com>
2023-11-29 12:46:58 -0400

    refactor: styles

--- src/routes/[id]/components/toolbox/appearance-editor.svelte
+++ src/routes/[id]/components/toolbox/appearance-editor.svelte
@@ -2,18 +2,22 @@
 	import { appearence } from '../../stores/general-appearance.store';
 </script>
 
-<div class="flex justify-end gap-2">
-	<label for="padding" class="mr-8 text-neutral-300">Padding:</label>
-	<input
-		type="range"
-		name="padding"
-		id="padding"
-		min="0"
-		max="100"
-		class="w-full"
-		bind:value={$appearence.padding}
-	/>
-	<output class="tabular-nums">
-		{$appearence.padding.toString(10).padStart(3, '0')}px
-	</output>
+<div class="flex flex-col gap-4">
+	<div class="flex flex-col">
+		<label for="padding" class="text-neutral-300">Padding:</label>
+		<div class="flex gap-4">
+			<input
+				type="range"
+				name="padding"
+				id="padding"
+				min="0"
+				max="100"
+				class="w-full"
+				bind:value={$appearence.padding}
+			/>
+			<output class="tabular-nums">
+				{$appearence.padding.toString(10).padStart(3, '0')}px
+			</output>
+		</div>
+	</div>
 </div>
