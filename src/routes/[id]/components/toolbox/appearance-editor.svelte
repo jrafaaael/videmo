27be33c1e3fe69c6bafd8312d294483220b91133
jrafaaael <jrmd070102@gmail.com>
2023-12-03 09:25:26 -0400

    refactor: styling

--- src/routes/[id]/components/toolbox/appearance-editor.svelte
+++ src/routes/[id]/components/toolbox/appearance-editor.svelte
@@ -3,21 +3,19 @@
 </script>
 
 <div class="flex flex-col gap-4">
-	<div class="flex flex-col">
-		<label for="padding" class="text-neutral-300">Padding:</label>
-		<div class="flex gap-4">
-			<input
-				type="range"
-				name="padding"
-				id="padding"
-				min="0"
-				max="100"
-				class="w-full"
-				bind:value={$appearence.padding}
-			/>
-			<output class="tabular-nums">
-				{$appearence.padding.toString(10).padStart(3, '0')}px
-			</output>
-		</div>
+	<div class="flex justify-between gap-4">
+		<label for="padding" class="text-neutral-300">Padding</label>
+		<output class="tabular-nums">
+			{$appearence.padding.toString(10).padStart(3, '0')}<span class="text-xs">px</span>
+		</output>
 	</div>
+	<input
+		type="range"
+		name="padding"
+		id="padding"
+		min="0"
+		max="100"
+		class="w-full"
+		bind:value={$appearence.padding}
+	/>
 </div>
