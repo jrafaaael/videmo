16c417e385cdee105986039aa603e94753ef1070
jrafaaael <jrmd070102@gmail.com>
2023-12-08 10:51:01 -0400

    refactor: record screen in web worker. create an mp4 instead of webm blob

--- src/routes/(recorder)/utils/create-screen-recorder.ts
+++ src/routes/(recorder)/utils/create-screen-recorder.ts
@@ -1,6 +1,8 @@
 import { writable } from 'svelte/store';
 import type { Recording } from '$lib/types/recording';
+import EncodeWorker from '../workers/encode.worker?worker';
 import { getBlobDuration } from './get-blob-duration';
+import { BPS, FPS } from './constants';
 
 interface Params {
 	onEnd?: (recording: Recording) => void;
@@ -9,40 +11,56 @@ interface Params {
 
 export function createScreenRecorder(params?: Params) {
 	const { subscribe, set } = writable({ isRecording: false });
-	const chunks: Blob[] = [];
+	let worker: Worker;
 	let stream: MediaStream;
 	let recorder: MediaRecorder;
 
 	async function start() {
 		try {
-			stream = await navigator.mediaDevices.getDisplayMedia();
-			recorder = new MediaRecorder(stream, {
-				mimeType: 'video/webm'
+			worker = new EncodeWorker();
+			stream = await navigator.mediaDevices.getDisplayMedia({
+				video: {
+					frameRate: FPS
+				}
 			});
+			recorder = new MediaRecorder(stream, {
+				mimeType: 'video/webm',
+				videoBitsPerSecond: BPS
+			});
+
+			worker.addEventListener('message', async (e) => {
+				if (e.data.type === 'result') {
+					const url = e.data.url;
+					const duration = await getBlobDuration(url);
+
+					params?.onEnd?.({
+						id: stream?.id,
+						url,
+						duration
+					});
+				}
+			});
+
+			recorder.addEventListener('stop', () => {
+				stream?.getTracks().forEach((track) => track.stop());
+				worker.postMessage({ type: 'end' });
+			});
+
+			const track = stream.getVideoTracks()[0];
+			const trackProcessor = new MediaStreamTrackProcessor({ track });
+			const trackStream: ReadableStream = trackProcessor.readable;
+			const trackSettings = track.getSettings();
 
 			recorder.start();
+			worker.postMessage(
+				{
+					type: 'start',
+					trackStream,
+					trackSettings
+				},
+				[trackStream]
+			);
 			set({ isRecording: true });
-
-			recorder.addEventListener('dataavailable', ({ data }) => chunks.push(data));
-
-			recorder.addEventListener('stop', async () => {
-				const videoBlob = new Blob(chunks, {
-					type: 'video/webm'
-				});
-				const url = URL.createObjectURL(videoBlob);
-				const duration = await getBlobDuration(url);
-
-				params?.onEnd?.({
-					id: stream?.id,
-					url,
-					duration
-				});
-			});
-
-			stream.getTracks().forEach((track) => {
-				track.addEventListener('ended', () => stop());
-			});
-
 			params?.onStart?.();
 		} catch (error) {
 			console.log(error);
@@ -51,7 +69,6 @@ export function createScreenRecorder(params?: Params) {
 
 	function stop() {
 		recorder?.stop();
-		stream?.getTracks().forEach((track) => track.stop());
 		set({ isRecording: false });
 	}
 
