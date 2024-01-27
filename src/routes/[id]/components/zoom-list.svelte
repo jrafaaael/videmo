592db6494a8245ae8b31c30c785d0b6b540fb96c
jrafaaael <jrmd070102@gmail.com>
2024-01-26 20:30:10 -0400

    chore: remove unused code

--- src/routes/[id]/components/zoom-list.svelte
+++ src/routes/[id]/components/zoom-list.svelte
@@ -2,25 +2,8 @@
 	import { recording } from '$lib/stores/recording.store';
 	import { zoomList } from '../stores/zoom-list.store';
 	import Zoom from './zoom.svelte';
-
-	function handleAddZoom() {
-		zoomList.addZoom({
-			id: new Date().getTime(),
-			start: 0,
-			end: $recording?.duration
-		});
-	}
 </script>
 
-<svelte:document
-	on:dblclick={() => {
-		console.log($zoomList);
-		zoomList._clear();
-	}}
-/>
-
-<!-- svelte-ignore a11y-no-static-element-interactions -->
-<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="w-full h-10 relative">
 	{#each $zoomList as zoom, idx (zoom.id)}
 		{@const width = ((zoom.end - zoom.start) * 100) / $recording?.duration}
