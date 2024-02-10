56109d92eb77562c5cf93c83d5f3444ad58f1cc8
jrafaaael <jrmd070102@gmail.com>
2024-02-10 08:08:06 -0400

    perf: prevent update zoom when `start` and `end` are the same as previous values

--- src/routes/[id]/components/zoom-list.svelte
+++ src/routes/[id]/components/zoom-list.svelte
@@ -20,44 +20,52 @@
 				const constrains = refToElement.parentElement.getBoundingClientRect();
 
 				if (direction === 'right') {
-					const endAt = (
+					const end = +(
 						((zoomRect.right + delta - constrains.left) * $recording.duration) /
 						(constrains.right - constrains.left)
 					).toFixed(2);
+
+					if (end === zoom.end) {
+						return;
+					}
 
 					/*
 					 * `resize` event aren't fired on every size change. when `endAt` is calculated, it can has a value greater than `nextZoom.start`
 					 * so current edited zoom couldn't be set to end exactly when next one starts. when this happens, I set the `end` value of
 					 * the current edited zoom equals to `start` of next one
 					 */
-					if (!nextZoom || +endAt < nextZoom.start) {
+					if (!nextZoom || end < nextZoom.start) {
 						zoomList.updateZoomById({
 							...zoom,
-							end: +endAt
+							end
 						});
-					} else if (+endAt > nextZoom?.start) {
+					} else if (end > nextZoom?.start) {
 						zoomList.updateZoomById({
 							...zoom,
 							end: +nextZoom?.start.toFixed(2)
 						});
 					}
 				} else if (direction === 'left') {
-					const startAt = (
+					const start = +(
 						((zoomRect.left - delta - constrains.left) * $recording.duration) /
 						(constrains.right - constrains.left)
 					).toFixed(2);
+
+					if (start === zoom.start) {
+						return;
+					}
 
 					/*
 					 * `resize` event aren't fired on every size change. when `startAt` is calculated, it can has a value less than `prevZoom.end`
 					 * so current edited zoom couldn't be set to start exactly when previous one ends. when this happens, I set the `start` value of
 					 * the current edited zoom equals to `end` of previous one
 					 */
-					if (!prevZoom || +startAt > prevZoom.end) {
+					if (!prevZoom || start > prevZoom.end) {
 						zoomList.updateZoomById({
 							...zoom,
-							start: +startAt
+							start
 						});
-					} else if (+startAt < prevZoom.end) {
+					} else if (start < prevZoom.end) {
 						zoomList.updateZoomById({
 							...zoom,
 							start: +prevZoom?.end.toFixed(2)
