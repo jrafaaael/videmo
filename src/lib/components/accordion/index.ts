4da550cc9cc530cffd547bcc83ca97b00e98c570
jrafaaael <jrmd070102@gmail.com>
2024-02-10 22:27:19 -0400

    refactor: replace `Accordion` component to `details` html tag

--- src/lib/components/accordion/index.ts
+++
@@ -1,6 +0,0 @@
-import Root from './root.svelte';
-import Item from './item.svelte';
-import Trigger from './trigger.svelte';
-import Content from './content.svelte';
-
-export { Root, Item, Trigger, Content };
--- src/routes/[id]/components/toolbox/toolbox.svelte
+++ src/routes/[id]/components/toolbox/toolbox.svelte
@@ -1,23 +1,18 @@
 <script lang="ts">
-	import * as Accordion from '$lib/components/accordion';
-	import ChevronDown from '../icons/chevron-down.filled.svelte';
 	import BackgroundEditor from './background-editor.svelte';
 	import AppearanceEditor from './appearance-editor.svelte';
 	import ZoomEditor from './zoom-editor.svelte';
 
 	const OPTIONS = [
 		{
-			id: 'background',
 			label: 'Background',
 			content: BackgroundEditor
 		},
 		{
-			id: 'appearance',
 			label: 'Appearance',
 			content: AppearanceEditor
 		},
 		{
-			id: 'zoom',
 			label: 'Zoom',
 			content: ZoomEditor
 		}
@@ -27,24 +22,31 @@
 <aside
 	class="min-w-[350px] max-w-full bg-neutral-900 border-r-2 border-r-white/5 basis-1/5 overflow-y-scroll"
 >
-	<Accordion.Root defaultValue={OPTIONS.at(0)?.id}>
-		<div class="w-full">
-			{#each OPTIONS as item}
-				<Accordion.Item class="w-full border-b border-b-white/10" id={item.id}>
-					<Accordion.Trigger
-						class="w-full h-16 px-4 flex justify-start items-center gap-2 relative"
-						id={item.id}
-					>
-						<div class="w-4 aspect-square text-neutral-500">
-							<ChevronDown />
-						</div>
-						{item.label}
-					</Accordion.Trigger>
-					<Accordion.Content class="px-4 pb-4" id={item.id}>
-						<svelte:component this={item.content} />
-					</Accordion.Content>
-				</Accordion.Item>
-			{/each}
-		</div>
-	</Accordion.Root>
+	{#each OPTIONS as option}
+		<details class="w-full border-b border-b-white/10">
+			<summary class="w-full p-5 font-medium flex justify-between items-center cursor-pointer list-none">
+				<span>
+					{option.label}
+				</span>
+			</summary>
+			<div class="px-5 pb-5">
+				<svelte:component this={option.content} />
+			</div>
+		</details>
+	{/each}
 </aside>
+
+<style>
+	/* https://stackoverflow.com/questions/73195948/change-color-fill-of-a-svg-path-when-used-in-the-content-of-a-before-pseudo-el */
+	summary::after {
+		content: '';
+		width: 16px;
+		aspect-ratio: 1;
+		background-color: rgba(250, 250, 250, 0.8);
+		mask: url($lib/assets/images/icons/plus.svg);
+	}
+
+	details[open] > summary::after {
+		mask: url($lib/assets/images/icons/minus.svg);
+	}
+</style>
