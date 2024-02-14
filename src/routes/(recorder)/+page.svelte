9f78ac42deb11919744db59692672e991948d24b
jrafaaael <jrmd070102@gmail.com>
2024-02-14 19:06:18 -0400

    fix: prevent card grow when few recordings

--- src/routes/(recorder)/+page.svelte
+++ src/routes/(recorder)/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <main
-	class="w-full h-full p-8 pb-32 grid grid-cols-[repeat(auto-fit,minmax(360px,1fr))] auto-rows-min gap-8 md:p-16 md:pb-32 lg:px-32 lg:gap-y-16"
+	class="w-full h-full p-8 pb-32 grid grid-cols-[repeat(auto-fill,minmax(360px,1fr))] auto-rows-min gap-8 md:p-16 md:pb-32 lg:px-32 lg:gap-y-16"
 >
 	<RecordingList />
 	<FloatingRecordingBar />
