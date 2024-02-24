abe1b4d6191a1bd2d1aace97579cc6486c0e841b
jrafaaael <jrmd070102@gmail.com>
2024-02-23 20:40:14 -0400

    refactor: improve countdown ui

--- src/routes/(recorder)/components/floating-recording-bar.svelte
+++ src/routes/(recorder)/components/floating-recording-bar.svelte
@@ -5,10 +5,11 @@
 	import VideoSlash from './icons/video-slash.svelte';
 	import Video from './icons/video.svelte';
 
+	const COUNTDOWN = 3000;
 	let countdown = 3000;
 	let interval: number | null = null;
 	const { start, stop } = recordScreen({
-		countdown,
+		countdown: COUNTDOWN,
 		onStart() {
 			interval = setInterval(() => (countdown -= 1000), 1000);
 		},
@@ -39,16 +40,32 @@
 </script>
 
 <footer
-	class="w-auto h-11 bg-neutral-900 border-2 border-white/10 rounded-full flex items-center gap-2 fixed bottom-20 left-1/2 -translate-x-1/2"
+	class="w-auto h-11 rounded-full flex items-center gap-2 fixed bottom-20 left-1/2 -translate-x-1/2 {!interval
+		? 'bg-neutral-900 border-2 border-white/10'
+		: 'bg-red-600 border-2 border-red-500/70'}"
 >
-	<button class="h-full px-4 flex items-center gap-2" on:click={handleClick}>
-		<span class="w-4 aspect-square">
-			{#if $isRecording}
-				<VideoSlash />
-			{:else}
-				<Video />
-			{/if}
-		</span>
-		<span>{$isRecording ? 'Stop' : 'Start'} recording {countdown}</span>
-	</button>
+	{#if !interval}
+		<button class="h-full px-4 flex items-center gap-2" on:click={handleClick}>
+			<span class="w-4 aspect-square">
+				{#if $isRecording}
+					<VideoSlash />
+				{:else}
+					<Video />
+				{/if}
+			</span>
+			<span>{$isRecording ? 'Stop' : 'Start'} recording</span>
+		</button>
+	{:else}
+		<div class="h-full px-4 flex items-center gap-12">
+			{#each Array.from({ length: COUNTDOWN / 1000 }, (_, i) => i + 1).reverse() as second}
+				<span
+					class="text-2xl font-bold {second >= countdown / 1000
+						? 'text-neutral-50'
+						: 'text-neutral-50/40'}"
+				>
+					{second}
+				</span>
+			{/each}
+		</div>
+	{/if}
 </footer>
