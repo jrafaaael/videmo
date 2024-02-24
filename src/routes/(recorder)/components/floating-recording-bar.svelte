82fbab1b9b6d5bcd7b1c8bd6e6bc7205e3df7005
jrafaaael <jrmd070102@gmail.com>
2024-02-24 16:04:12 -0400

    feat: add recording duration

--- src/routes/(recorder)/components/floating-recording-bar.svelte
+++ src/routes/(recorder)/components/floating-recording-bar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { writable } from 'svelte/store';
 	import { goto } from '$app/navigation';
+	import { secondsToTime } from '$lib/utils/seconds-to-time';
 	import * as Select from '$lib/components/select';
 	import ChevronDown from '$lib/components/icons/chevron-down.svelte';
 	import Check from '$lib/components/icons/check.svelte';
@@ -12,17 +13,23 @@
 	import Clock from './icons/clock.svelte';
 
 	const countdownDuration = writable(COUNTDOWN_OPTIONS.at(0));
-	let interval: number | null = null;
+	let countdownInterval: number | null = null;
+	let recordingStartDate: Date | null = null;
+	let now: Date | null = null;
+	let recordingDurationInterval: number | null = null;
 	const { start, stop } = recordScreen({
 		onStart() {
-			interval = setInterval(() => (countdown -= 1000), 1000);
+			countdownInterval = setInterval(() => (countdown -= 1000), 1000);
 		},
 		async onEnd(data) {
+			clearInterval(recordingDurationInterval!);
+			recordingDurationInterval = null;
 			$isRecording = false;
 			await goto(data.folder);
 		}
 	});
 	$: countdown = $countdownDuration.value;
+	$: recordingDuration = (now?.getTime() ?? 0) - (recordingStartDate?.getTime() ?? 0);
 
 	async function handleClick() {
 		if ($isRecording) {
@@ -32,6 +39,9 @@
 
 		try {
 			await start({ countdown });
+			recordingStartDate = new Date();
+			now = new Date();
+			recordingDurationInterval = setInterval(() => (now = new Date()), 1000);
 			$isRecording = true;
 		} catch (error) {
 			console.error(error);
@@ -39,17 +49,17 @@
 	}
 
 	$: if (countdown <= 0) {
-		clearInterval(interval!);
-		interval = null;
+		clearInterval(countdownInterval!);
+		countdownInterval = null;
 	}
 </script>
 
 <footer
-	class="w-auto h-12 rounded-full flex items-center fixed bottom-20 left-1/2 -translate-x-1/2 {!interval
+	class="w-auto h-12 rounded-full flex items-center fixed bottom-20 left-1/2 -translate-x-1/2 {!countdownInterval
 		? 'bg-neutral-900 border-2 border-white/10'
 		: 'bg-red-600 border-2 border-red-500/70'}"
 >
-	{#if !interval}
+	{#if !countdownInterval}
 		<button class="h-full px-5 flex items-center gap-2" on:click={handleClick}>
 			<span class="w-4 aspect-square text-neutral-50/50">
 				{#if $isRecording}
@@ -61,44 +71,48 @@
 			<span>{$isRecording ? 'Stop' : 'Start'} recording</span>
 		</button>
 		<div class="w-[2px] h-[40%] bg-white/20" />
-		<Select.Root
-			selected={countdownDuration}
-			defaultSelected={$countdownDuration}
-			positioning={{ placement: 'top', gutter: 5 }}
-		>
-			<Select.Trigger class="h-full px-5 flex items-center gap-2">
-				<span class="w-4 aspect-square text-neutral-50/50">
-					<Clock />
-				</span>
-				<span>
-					<span>Countdown:</span>
-					<output class="font-bold tabular-nums">{$countdownDuration?.value / 1000}</output><span
-						class="text-sm font-bold">s</span
-					>
-				</span>
-				<span class="w-4 aspect-square rotate-180">
-					<ChevronDown />
-				</span>
-			</Select.Trigger>
-			<Select.Menu
-				class="bg-neutral-800/80 backdrop-blur-lg border-2 border-white/5 rounded-md flex flex-col gap-1 overflow-hidden"
+		{#if $isRecording}
+			<output class="px-5 tabular-nums">{secondsToTime(recordingDuration / 1000)}</output>
+		{:else}
+			<Select.Root
+				selected={countdownDuration}
+				defaultSelected={$countdownDuration}
+				positioning={{ placement: 'top', gutter: 5 }}
 			>
-				{#each [...COUNTDOWN_OPTIONS].reverse() as option}
-					<Select.Option
-						let:isSelected
-						class="w-36 py-1 px-4 flex justify-between items-center gap-4 cursor-pointer data-[highlighted]:bg-white/10 data-[disabled]:text-neutral-400 data-[disabled]:cursor-not-allowed"
-						{...option}
-					>
-						<span class={isSelected ? 'font-bold' : 'font-normal'}>{option.label}</span>
-						{#if isSelected}
-							<span class="w-3 aspect-square">
-								<Check />
-							</span>
-						{/if}
-					</Select.Option>
-				{/each}
-			</Select.Menu>
-		</Select.Root>
+				<Select.Trigger class="h-full px-5 flex items-center gap-2">
+					<span class="w-4 aspect-square text-neutral-50/50">
+						<Clock />
+					</span>
+					<span>
+						<span>Countdown:</span>
+						<output class="font-bold tabular-nums">{$countdownDuration?.value / 1000}</output><span
+							class="text-sm font-bold">s</span
+						>
+					</span>
+					<span class="w-4 aspect-square rotate-180">
+						<ChevronDown />
+					</span>
+				</Select.Trigger>
+				<Select.Menu
+					class="bg-neutral-800/80 backdrop-blur-lg border-2 border-white/5 rounded-md flex flex-col gap-1 overflow-hidden"
+				>
+					{#each [...COUNTDOWN_OPTIONS].reverse() as option}
+						<Select.Option
+							let:isSelected
+							class="w-36 py-1 px-4 flex justify-between items-center gap-4 cursor-pointer data-[highlighted]:bg-white/10 data-[disabled]:text-neutral-400 data-[disabled]:cursor-not-allowed"
+							{...option}
+						>
+							<span class={isSelected ? 'font-bold' : 'font-normal'}>{option.label}</span>
+							{#if isSelected}
+								<span class="w-3 aspect-square">
+									<Check />
+								</span>
+							{/if}
+						</Select.Option>
+					{/each}
+				</Select.Menu>
+			</Select.Root>
+		{/if}
 	{:else}
 		<div class="min-w-[100px] h-full px-4 flex justify-center items-center gap-12">
 			{#each Array.from({ length: $countdownDuration.value / 1000 }, (_, i) => i + 1).reverse() as second}
