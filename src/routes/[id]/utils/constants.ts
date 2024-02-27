3dcd3f2dddbf491fa1de1850e386d9363e9c4636
jrafaaael <jrmd070102@gmail.com>
2024-02-27 07:24:39 -0400

    feat: add a bunch of new apple wallpapers

--- src/routes/[id]/utils/constants.ts
+++ src/routes/[id]/utils/constants.ts
@@ -1,7 +1,19 @@
 import bigSur from '$lib/assets/images/wallpapers/big-sur.webp';
+import bigSurDark from '$lib/assets/images/wallpapers/big-sur-dark.webp';
 import monterey from '$lib/assets/images/wallpapers/monterey.webp';
+import montereyBlack from '$lib/assets/images/wallpapers/monterey-black.webp';
+import montereyBlue from '$lib/assets/images/wallpapers/monterey-blue.webp';
+import montereyDark from '$lib/assets/images/wallpapers/monterey-dark.webp';
+import montereyGreen from '$lib/assets/images/wallpapers/monterey-green.webp';
+import montereyOrange from '$lib/assets/images/wallpapers/monterey-orange.webp';
 import sonoma from '$lib/assets/images/wallpapers/sonoma.webp';
+import sonomaDark from '$lib/assets/images/wallpapers/sonoma-dark.webp';
+import stock16 from '$lib/assets/images/wallpapers/stock-ipados-16.webp';
+import stock16Dark from '$lib/assets/images/wallpapers/stock-ipados-16-dark.webp';
+import stock17_1 from '$lib/assets/images/wallpapers/stock-ipados-17-1.webp';
+import stock17_2 from '$lib/assets/images/wallpapers/stock-ipados-17-2.webp';
 import ventura from '$lib/assets/images/wallpapers/ventura.webp';
+import venturaDark from '$lib/assets/images/wallpapers/ventura-dark.webp';
 
 export const MICROSECONDS_PER_SECOND = 1_000_000;
 export const MIN_ZOOM_LEVEL = 1;
@@ -10,9 +22,21 @@ export const ZOOM_TRANSITION_DURATION = 0.25;
 
 export const WALLPAPERS = [
 	{ name: 'bigSur', url: bigSur },
+	{ name: 'bigSurDark', url: bigSurDark },
 	{ name: 'monterey', url: monterey },
+	{ name: 'montereyBlack', url: montereyBlack },
+	{ name: 'montereyBlue', url: montereyBlue },
+	{ name: 'montereyDark', url: montereyDark },
+	{ name: 'montereyGreen', url: montereyGreen },
+	{ name: 'montereyOrange', url: montereyOrange },
 	{ name: 'sonoma', url: sonoma },
+	{ name: 'sonomaDark', url: sonomaDark },
+	{ name: 'stock16', url: stock16 },
+	{ name: 'stock16Dark', url: stock16Dark },
+	{ name: 'stock17_1', url: stock17_1 },
+	{ name: 'stock17_2', url: stock17_2 },
 	{ name: 'ventura', url: ventura },
+	{ name: 'venturaDark', url: venturaDark }
 ] as const;
 
 export type WallpaperNames = typeof WALLPAPERS[number]['name'];
