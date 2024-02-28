7bf54584b3ccf6ba2836900bafc44132456df0d4
jrafaaael <jrmd070102@gmail.com>
2024-02-27 20:04:58 -0400

    fix: allow backgrounds grow all possible width

--- src/routes/[id]/components/toolbox/background-editor.svelte
+++ src/routes/[id]/components/toolbox/background-editor.svelte
@@ -3,18 +3,14 @@
 	import { WALLPAPERS } from '../../utils/constants';
 </script>
 
-<div class="flex flex-wrap gap-4">
+<div class="grid grid-cols-[repeat(auto-fill,minmax(40px,1fr))] gap-4">
 	{#each WALLPAPERS as wallpaper}
 		<label
 			class="rounded-lg overflow-hidden outline outline-2 outline-offset-2 cursor-pointer transition-transform hover:scale-95 focus-within:scale-95
 			{$background.name === wallpaper.name ? 'outline-white/90 hover:scale-100' : 'outline-transparent'}"
 			for={wallpaper.name}
 		>
-			<img
-				class="w-10 aspect-square object-fill"
-				src={wallpaper.url}
-				alt="{wallpaper.name} wallpaper"
-			/>
+			<img class="aspect-square object-fill" src={wallpaper.url} alt="{wallpaper.name} wallpaper" />
 			<input
 				type="radio"
 				class="sr-only"
