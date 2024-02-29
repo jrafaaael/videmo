7b35ffd91c32efd492f80365b26f18bcb37609fe
jrafaaael <jrmd070102@gmail.com>
2024-02-28 21:38:33 -0400

    fix: checked style

--- src/routes/[id]/components/toolbox/background/color-picker.svelte
+++ src/routes/[id]/components/toolbox/background/color-picker.svelte
@@ -6,11 +6,12 @@
 <div class="grid grid-cols-[repeat(auto-fill,minmax(40px,1fr))] gap-4">
 	{#each COLORS as color}
 		<label
-			class="rounded-lg overflow-hidden outline outline-2 outline-offset-2 cursor-pointer transition-transform hover:scale-95 focus-within:scale-95"
+			class="rounded-lg overflow-hidden outline outline-2 outline-transparent outline-offset-2 cursor-pointer transition-transform hover:scale-95 focus-within:scale-95 has-[:checked]:outline-white/90 has-[:checked]:hover:scale-100"
 			for={color}
 		>
 			<div class="aspect-square object-fill" style="background-color: {color};" />
 			<input
+				checked={color === $background.name}
 				type="radio"
 				class="sr-only"
 				name="wallpaper"
--- src/routes/[id]/components/toolbox/background/image-picker.svelte
+++ src/routes/[id]/components/toolbox/background/image-picker.svelte
@@ -6,12 +6,12 @@
 <div class="grid grid-cols-[repeat(auto-fill,minmax(40px,1fr))] gap-4">
 	{#each WALLPAPERS as wallpaper}
 		<label
-			class="rounded-lg overflow-hidden outline outline-2 outline-offset-2 cursor-pointer transition-transform hover:scale-95 focus-within:scale-95
-					{$background.name === wallpaper.name ? 'outline-white/90 hover:scale-100' : 'outline-transparent'}"
+			class="rounded-lg overflow-hidden outline outline-2 outline-transparent outline-offset-2 cursor-pointer transition-transform hover:scale-95 focus-within:scale-95 has-[:checked]:outline-white/90 has-[:checked]:hover:scale-100"
 			for={wallpaper.name}
 		>
 			<img class="aspect-square object-fill" src={wallpaper.url} alt="{wallpaper.name} wallpaper" />
 			<input
+				checked={wallpaper.name === $background.name}
 				type="radio"
 				class="sr-only"
 				name="wallpaper"
