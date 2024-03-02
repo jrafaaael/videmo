3ea7f6cda3b36d20b04c897e88ca7b9ad7d8016f
jrafaaael <jrmd070102@gmail.com>
2024-03-01 21:44:40 -0400

    refactor: styling

--- src/routes/[id]/components/toolbox/background-editor.svelte
+++ src/routes/[id]/components/toolbox/background-editor.svelte
@@ -5,16 +5,16 @@
 </script>
 
 <Tabs.Root>
-	<Tabs.List class="mb-8 flex justify-between gap-4">
+	<Tabs.List class="mb-8 p-1 rounded-lg bg-white/5 text-neutral-300 flex justify-between gap-4">
 		<Tabs.Trigger
 			id="image"
-			class="py-[6px] px-3 rounded-md w-full data-[state=active]:bg-white/10"
+			class="py-[6px] px-3 border border-transparent rounded-md w-full data-[state=active]:bg-white/5 data-[state=active]:border-white/[0.025]"
 		>
 			Image
 		</Tabs.Trigger>
 		<Tabs.Trigger
 			id="color"
-			class="py-[6px] px-3 rounded-md w-full data-[state=active]:bg-white/10"
+			class="py-[6px] px-3 border border-transparent rounded-md w-full data-[state=active]:bg-white/5 data-[state=active]:border-white/[0.025]"
 		>
 			Color
 		</Tabs.Trigger>
