9157d36986fb297ace80d31f1bbd573a35ce090b
jrafaaael <jrmd070102@gmail.com>
2024-03-01 21:05:11 -0400

    refactor: styling

--- src/routes/[id]/components/toolbox/background/color-picker.svelte
+++ src/routes/[id]/components/toolbox/background/color-picker.svelte
@@ -44,7 +44,7 @@
 			class="rounded-lg overflow-hidden outline outline-2 outline-transparent outline-offset-2 cursor-pointer transition-transform hover:scale-95 focus-within:scale-95 has-[:checked]:outline-white/90 has-[:checked]:hover:scale-100"
 			for={color}
 		>
-			<div class="aspect-square object-fill" style="background-color: {color};" />
+			<div class="aspect-square" style="background-color: {color};" />
 			<input
 				checked={typeof $background === 'string' && color === $background}
 				type="radio"
