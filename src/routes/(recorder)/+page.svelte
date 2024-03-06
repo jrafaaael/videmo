0ad5ef411ecb4f19c3e8aa20593cb2ff1237cdb7
jrafaaael <jrmd070102@gmail.com>
2024-03-05 23:42:13 -0400

    feat: add overlay when dragging

--- src/routes/(recorder)/+page.svelte
+++ src/routes/(recorder)/+page.svelte
@@ -5,6 +5,8 @@
 	import RecordingList from './components/recording-list.svelte';
 	import FloatingRecordingBar from './components/floating-recording-bar.svelte';
 
+	let isDroppingNewRecording = false;
+
 	async function handleDrop({ detail: { files } }: CustomEvent<{ files: FileList | undefined }>) {
 		const file = files?.[0];
 
@@ -21,10 +23,23 @@
 
 <main>
 	<Dropzone
-		class="w-full min-h-dvh p-8 pb-44 grid grid-cols-[repeat(auto-fill,minmax(360px,1fr))] auto-rows-min gap-8 cursor-default md:p-16 md:pb-44 lg:px-32 lg:gap-y-16"
+		class="w-full min-h-dvh p-8 pb-44 grid grid-cols-[repeat(auto-fill,minmax(360px,1fr))] auto-rows-min gap-8 cursor-default md:p-16 md:pb-44 lg:px-32 lg:gap-y-16 {isDroppingNewRecording
+			? '*:pointer-events-none'
+			: ''}"
 		openPickerOnClick={false}
+		on:dragenter={() => (isDroppingNewRecording = true)}
+		on:dragover={() => (isDroppingNewRecording = true)}
+		on:dragleave={() => (isDroppingNewRecording = false)}
 		on:drop={handleDrop}
 	>
+		{#if isDroppingNewRecording}
+			<article
+				class="w-full h-full bg-white/10 flex justify-center items-center flex-col gap-2 fixed inset-0 z-10 backdrop-blur"
+			>
+				<h2 class="text-2xl font-bold">Drop it!</h2>
+				<p>Drop here any video to upload it and star editing!</p>
+			</article>
+		{/if}
 		<RecordingList />
 		<FloatingRecordingBar />
 	</Dropzone>
