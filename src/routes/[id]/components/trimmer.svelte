d5b91b8758c78091fbdd65007da838977ebbb37d
jrafaaael <jrmd070102@gmail.com>
2024-03-09 00:01:53 -0400

    wip: use `svelte-moveable` in trimmer

--- src/routes/[id]/components/trimmer.svelte
+++ src/routes/[id]/components/trimmer.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	import { edits } from '../stores/edits.store';
 	import { recording } from '../stores/recording.store';
 	import { videoStatus } from '../stores/video-status.store';
@@ -42,5 +42,39 @@
 				$videoStatus.currentTime = end;
 			}
 		}
+	}}
+/> -->
+
+<script lang="ts">
+	import Moveable from 'svelte-moveable';
+	// import { edits } from '../stores/edits.store';
+	// import { recording } from '../stores/recording.store';
+	// import { videoStatus } from '../stores/video-status.store';
+
+	export let isResizing: boolean;
+	let target: HTMLDivElement;
+	// $: width = (($edits.endAt - $edits.startAt) * 100) / $recording?.duration;
+	// $: left = ($edits.startAt * 100) / $recording?.duration;
+</script>
+
+<div class="h-10 bg-blue-500/30 rounded-md overflow-hidden" bind:this={target} />
+<Moveable
+	{target}
+	origin
+	resizable
+	throttleResize={0}
+	renderDirections={['e', 'w']}
+	on:resizeStart={({ detail }) => console.log(detail)}
+	on:resize={({ detail: e }) => {
+		const constrains = e.target.parentElement.getBoundingClientRect();
+		const trimmerRect = e.target.getBoundingClientRect();
+		console.log(e);
+
+		if (trimmerRect.left < constrains?.left) {
+			return;
+		}
+
+		e.target.style.width = `${e.width}px`;
+		e.target.style.transform = e.drag.transform;
 	}}
 />
