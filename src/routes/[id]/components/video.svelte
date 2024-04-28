a1334d22acc48df62f815ca6a4f74e73f286d1d5
jrafaaael <jrmd070102@gmail.com>
2024-04-28 00:07:52 -0400

    fix: find zoom on seek

--- src/routes/[id]/components/video.svelte
+++ src/routes/[id]/components/video.svelte
@@ -335,9 +335,18 @@
 			}
 		}}
 		on:seeked={() => {
-			$currentZoomIndex = $zooms.findIndex(
-				(zoom) => zoom.start >= $videoStatus.currentTime || zoom.end >= $videoStatus.currentTime
-			);
+			if (
+				$videoStatus.currentTime < $currentZoom?.start ||
+				$videoStatus.currentTime > $currentZoom?.end + ZOOM_TRANSITION_DURATION
+			) {
+				currentZoomLevel = 1;
+			}
+
+			if ($videoStatus.currentTime < $currentZoom?.start) {
+				$currentZoomIndex = Math.max(0, $currentZoomIndex - 1);
+			} else if ($videoStatus.currentTime > $currentZoom?.end + ZOOM_TRANSITION_DURATION) {
+				$currentZoomIndex = Math.min($zooms.length - 1, $currentZoomIndex + 1);
+			}
 
 			draw(videoRef, $videoStatus.currentTime);
 		}}
