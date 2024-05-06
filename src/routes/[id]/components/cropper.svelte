0591068ea748e4354bf63f4248818bc86b1a52e9
jrafaaael <jrmd070102@gmail.com>
2024-05-06 19:35:15 -0400

    feat: create `Cropper` svelte component from `cropperjs`

---
+++ src/routes/[id]/components/cropper.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Cropper from 'cropperjs';
+	import 'cropperjs/dist/cropper.min.css';
+
+	export let img: HTMLImageElement | HTMLCanvasElement;
+	let cropper = null;
+
+	onMount(() => {
+		cropper = new Cropper(img as HTMLImageElement, {
+			dragMode: 'none',
+			background: false,
+			movable: false,
+			rotatable: false,
+			scalable: false,
+			zoomable: false,
+			crop(event) {
+				console.log(event);
+			}
+		});
+	});
+</script>
