159e79151345f0c4a66efbc639eb410ee598e2b0
jrafaaael <jrmd070102@gmail.com>
2024-05-10 21:22:42 -0400

    fix: use canvas size when cropping recording

--- src/routes/[id]/components/crop-frame.svelte
+++ src/routes/[id]/components/crop-frame.svelte
@@ -10,12 +10,25 @@
 
 		const ctx = canvasRef.getContext('2d');
 
-		ctx?.drawImage($videoStatus.ref, 0, 0, 1920, 1080);
+		ctx?.drawImage(
+			$videoStatus.ref,
+			0,
+			0,
+			$videoStatus.ref.videoWidth,
+			$videoStatus.ref.videoHeight
+		);
 	});
 </script>
 
 <div class="max-w-full rounded-md overflow-hidden relative">
-	<canvas class="max-w-full block" width="1920" height="1080" bind:this={canvasRef} />
+	{#if $videoStatus.ref}
+		<canvas
+			class="max-w-full block"
+			width={$videoStatus.ref.videoWidth}
+			height={$videoStatus.ref.videoHeight}
+			bind:this={canvasRef}
+		/>
+	{/if}
 	{#if canvasRef}
 		<Cropper img={canvasRef} on:crop />
 	{/if}
