3ced29ccb5680e6ad234584d6ccb084605c73a73
jrafaaael <jrmd070102@gmail.com>
2024-07-06 20:19:58 -0400

    feat: extract video frames on demand

--- src/routes/[id]/utils/generate-final-video.ts
+++ src/routes/[id]/utils/generate-final-video.ts
@@ -1,5 +1,4 @@
 import { MP4Clip } from '@webav/av-cliper';
-import { MICROSECONDS_PER_SECOND } from './constants';
 
 interface ExtractVideoFramesParams {
 	url: string;
@@ -7,9 +6,10 @@ interface ExtractVideoFramesParams {
 
 type GenerateVideoParams = ExtractVideoFramesParams;
 
-async function extractVideoFrames({ url }: ExtractVideoFramesParams) {
+async function* extractVideoFrames({ url }: ExtractVideoFramesParams) {
 	const res = await fetch(url);
 	const video = new MP4Clip(res?.body);
+	let frames: VideoFrame[] = [];
 	let time = 0;
 
 	await video.ready;
@@ -18,21 +18,33 @@ async function extractVideoFrames({ url }: ExtractVideoFramesParams) {
 		const { state, video: frame } = await video.tick(time);
 
 		if (state === 'done') break;
-		if (frame !== null && state === 'success') frame?.close();
 		if (!frame) {
 			time += 100;
 
 			continue;
 		}
 
-		console.log(frame, time / MICROSECONDS_PER_SECOND);
+		frames.push(frame);
+
+		if (frames.length >= 10) {
+			yield frames;
+
+			frames = [];
+		}
 
 		time += frame?.duration ?? 0;
 	}
 
 	video.destroy();
+
+	yield frames;
 }
 
 export async function generateVideo({ url }: GenerateVideoParams) {
-	await extractVideoFrames({ url });
+	const extractor = extractVideoFrames({ url });
+
+	for await (const values of extractor) {
+		console.log(values);
+		values.forEach((v) => v.close());
+	}
 }
