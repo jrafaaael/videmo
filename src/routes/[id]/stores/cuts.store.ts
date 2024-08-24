226866c6f78d76c77f088fb00088ddb96d8cb057
jrafaaael <jrmd070102@gmail.com>
2024-08-24 15:57:08 -0400

    refactor: improve types

--- src/routes/[id]/stores/cuts.store.ts
+++ src/routes/[id]/stores/cuts.store.ts
@@ -24,4 +24,4 @@ function createCutStore() {
 
 export const cuts = createCutStore();
 export const currentCutIndex = writable(0);
-export const currentCut = derived([cuts, currentCutIndex], ([cuts, idx]) => cuts[idx] ?? null);
+export const currentCut = derived([cuts, currentCutIndex], ([cuts, idx]) => cuts.at(idx) ?? null);
