8e153eaa2adb1e88fbb2d6f866ba5fc8f4f214a6
jrafaaael <jrmd070102@gmail.com>
2025-01-11 17:14:08 -0400

    feat: add `ContextMenu` component

---
+++ src/lib/components/context-menu/root.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { CreateContextMenuProps } from '@melt-ui/svelte';
+	import { setContextMenuContext } from './context';
+
+	type $$Props = CreateContextMenuProps;
+
+	export let positioning: $$Props['positioning'] = undefined;
+
+	setContextMenuContext({
+		positioning,
+		preventScroll: false,
+	});
+</script>
+
+<slot />
---
+++ src/lib/components/context-menu/trigger.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { melt } from '@melt-ui/svelte';
+	import { getContextMenuContext } from './context';
+
+	const {
+		elements: { trigger }
+	} = getContextMenuContext();
+</script>
+
+<button use:melt={$trigger} {...$$restProps}>
+	<slot />
+</button>
