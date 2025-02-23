f6c8bad67023a437163ecc3636f0f33b5b11b255
jrafaaael <jrmd070102@gmail.com>
2025-02-22 23:22:28 -0400

    refactor: increase keyframe separation

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,4 +1,4 @@
 // https://developer.mozilla.org/en-US/docs/Web/Media/Formats/codecs_parameter#iso_base_media_file_format_mp4_quicktime_and_3gp
 export const CODEC = 'avc1.64002a';
 export const FPS = 60;
-export const KEYFRAME_SEPARATION_IN_SECONDS = 0.5;
+export const KEYFRAME_SEPARATION_IN_SECONDS = 2;
